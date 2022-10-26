311be6b7b3fed52d7bbbbc1613d7f3ef477a8681
emreihtiyar <emre.ihti1@gmail.com>
2022-10-26 18:42:55 +0300

    new architecture of Blocks

--- src/Block.py
+++ src/Block.py
@@ -1,19 +1,64 @@
 
+import enum
+import Geometry
 
-
-class Block:
+class BlockType(enum.Enum):
+    """Job status enum"""
+    PAGE = 0
+    LINE = 1
+    WORD = 2
+    KEY_VALUE_SET = 3
     
-    def __init__(self) -> None:
+    @classmethod
+    def from_str(cls, label):
+        if label in ('PAGE', 'page', 'Page'):
+            return cls.PAGE
+        elif label in ('LINE', 'line', 'Line'):
+            return cls.LINE
+        elif label in ('WORD', 'word', 'Word'):
+            return cls.WORD
+        elif label in ('KEY_VALUE_SET', 'key_value_set', 'Key_value_set'):
+            return cls.KEY_VALUE_SET
+        else:
+            raise NotImplementedError('Unknown block type: {}'.format(label))
+
+
+class TextType(enum.Enum):
+    "Text Type enum"
+    PRINTED = 0
+    HANDWRITTEN = 1
+
+
+
+class BaseBlock:
+    def __init__(self, id:str, block_type:BlockType, geometry:Geometry, relationships:list) -> None:
         self.id = None
         self.block_type = None
-        self.page = None
         self.geometry = None
-        self.confidence = None
-        self.text = None
-        self.row_index = None
-        self.column_index = None
-        self.row_span = None
-        self.column_span = None
-        self.entity_types = None
-        self.selection_status = None
         self.relationships = None
+
+
+class PageBlock(BaseBlock):
+    def __init__(self, id:str, block_type:BlockType, geometry:Geometry, relationships:list, page_number:int) -> None:
+        super().__init__(id, block_type, geometry, relationships)
+        self.page_number = page_number
+
+
+class LineBlock(BaseBlock):
+    def __init__(self, id:str, block_type:BlockType, geometry:Geometry, relationships:list,\
+                page:PageBlock ,text:str, confidence:float) -> None:
+        super().__init__(id, block_type, geometry, relationships)
+        self.text = text
+        self.confidence = confidence
+        self.page = page
+
+
+class WordBlock(BaseBlock):
+    def __init__(self, id:str, block_type:BlockType, geometry:Geometry, relationships:list,\
+                page:PageBlock, line:LineBlock, text:str,confidence:float, text_type:TextType) -> None:
+        super().__init__(id, block_type, geometry, relationships)
+        self.text = text
+        self.confidence = confidence
+        self.page = page
+        self.line = line
+        self.text_type = text_type
